d060d0ebf28b0d29a4d0b7f438530b79f72eb205
0xNunana <kudayapaul@gmail.com>
2024-02-01 17:36:39 +0000

    hero priority image

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
     <div className='flex justify-center items-center '>
     <div className='relative'>
         <div className='pb-5'>
-            <Image src='/profit.png' alt='profit' width={80} height={20} className='md:w-[100px]'/>
+            <Image src='/profit.png' alt='profit' width={80} height={20} priority className='md:w-[100px]'/>
         </div>
         <p className='font-semibold text-4xl lg:text-6xl max-sm:text-center'>We help your business grow <span className='italic underline text-0xblue font-normal'>exponentially.</span></p>
     <p className='py-7 md:py-10 text-gray-700 max-sm:text-center'>Like any great agency, we are only as good as the result we deliver </p>
