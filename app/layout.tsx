1e0489c85f8686d29306fcc54d1ea50394460f32
0xNunana <kudayapaul@gmail.com>
2024-03-18 21:21:46 +0000

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,19 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "0xNunana Labs | Brand Developemt",
+  title: "0xNunana Labs | Brand Development",
   description: "Brand development and advertisement",
+  keywords:'Branding, Web Design,SEO,SAAS,Software,Advertisement,Growth',
+  category: 'technology,education, science,advertisement',
+  metadataBase:new URL('https://0xnunanalabs.vercel.app/'),
+  openGraph:{
+    type: "website",
+    title: "0xNunana Labs",
+    description: "Brand development and advertisement",
+    siteName: "0xNunana Labs",
+    images:'/nunana.webp'
+  
+  }
 };
 
 export default function RootLayout({
